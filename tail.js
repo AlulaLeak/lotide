4e7c39bac65105c97dfc7ec2c0de768f90092344
AlulaLeak <alula.leakemariam94@gmail.com>
2021-11-23 07:30:44 +0000

    tails

---
+++ tail.js
@@ -0,0 +1,30 @@
+
+const tail = (arr) => {
+
+  let tailArray = []
+  for (let i = 1; i < arr.length; i++) {
+    tailArray.push(arr[i])
+    
+  }
+
+  return tailArray
+}
+
+const assertEqual = function (actual, expected) {
+
+  let answer = "";
+
+  if (actual === expected) {
+    answer = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+
+  } else if (actual !== expected) {
+    answer = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+
+  return console.log(answer);
+};
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
