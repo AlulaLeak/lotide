d5a997669a2497f0f0a7e52e9a29695c994cb3d0
AlulaLeak <alula.leakemariam94@gmail.com>
2021-11-23 07:18:32 +0000

    head.js

---
+++ head.js
@@ -0,0 +1,35 @@
+
+const head = (arr) => {
+  
+  return arr[0];
+
+};
+
+
+
+
+const assertEqual = function(actual, expected) {
+
+
+
+
+
+
+  
+  let answer = "";
+
+  if (actual === expected) {
+    answer = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+
+  } else if (actual !== expected) {
+    answer = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+
+  return console.log(answer);
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
