0153601a279148a1848c9bda8665a5cb70bf984c
AlulaLeak <alula.leakemariam94@gmail.com>
2021-11-23 05:37:55 +0000

    assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+
+  let answer = "";
+
+  if (actual === expected) {
+    answer = '✅✅✅ Assertion Passed: ' + actual + ' === ' + expected;
+
+  } else if (actual !== expected) {
+    answer = '🛑🛑🛑 Assertion Failed: ' + actual + ' !== ' + expected;
+  }
+
+  return console.log(answer);
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 2);
