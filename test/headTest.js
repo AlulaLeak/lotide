d721916989c3e8e37a96a70e2e1bbc81c663b67a
AlulaLeak <alula.leakemariam94@gmail.com>
2021-11-30 17:57:25 +0000

    index.js added

--- test/headTest.js
+++ test/headTest.js
@@ -10,12 +10,3 @@ describe("#head", () => {
   });
 
 });
-
-/*
-const head = require('../head');
-const assertEqual = require('../assertEqual');
-
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-*/
